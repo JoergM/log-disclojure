93c8371ef3bd09970225bb9574632e2800adf771
Joerg Mueller <joergm42.gmail.com>
2013-05-03 16:19:53 +0200

    Implemented first version of log-file parsing.

--- src/log_disclojure/core.clj
+++ src/log_disclojure/core.clj
@@ -1,6 +1,11 @@
 (ns log-disclojure.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn from-file [path]
+  (clojure.string/split-lines (slurp path)))
+
+(defn parse-to-event [raw-event]
+  {:timestamp (subs raw-event 0 23)
+    :content (subs raw-event 24)})
+
+(defn load-events [path]
+  (map parse-to-event (from-file path)))
--- test/log_disclojure/core_test.clj
+++ test/log_disclojure/core_test.clj
@@ -2,6 +2,27 @@
   (:use clojure.test
         log-disclojure.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest datei-einlesen
+  (testing "Datei wird eingelesen und gesliced"
+    (is (= (count (from-file "data/log.out")) 20))))
+
+(deftest datei-einlesen-und-parsen
+  (testing "Datei wird eingelesen und geparsed"
+    (is (= (load-events "data/log.out") (read-string (slurp "data/log-result.clj"))))))
+
+(deftest parse-to-event-test
+  (is (= (parse-to-event "2013-05-03 13:30:17,955 INFO  myApp-2013.05.03.12.27.49")
+        {:timestamp "2013-05-03 13:30:17,955" :content "INFO  myApp-2013.05.03.12.27.49"})))
+
+;; Gesamtrahmen
+;; Liste von Dateinen eingeben und eine Sequenz von Events ausgeben,
+;; wobei ein Event aus Datum und Rest besteht
+
+(comment
+
+(deftest integration
+  (main "data/log1.out" "data/log2.out"))
+
+)
+
+(run-tests)
